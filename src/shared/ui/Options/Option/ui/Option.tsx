a0a696412a9853b19036b43d7430b6409f746d17
pure_mind <pure_mind@list.ru>
2023-01-28 01:10:29 +0300

    1. change architecture FSD 2. autocompleteViewModel now use InputViewModel 3. Add MoreAutocomplete and LessAutocomplete components 4. remove much any types

--- src/shared/ui/Options/Option/ui/Option.tsx
+++ src/shared/ui/Options/Option/ui/Option.tsx
@@ -7,7 +7,11 @@ export const Option = ({ option, ...otherProps }: any) => {
         <span className={styles.option__title}>{option.name}</span>
         <span className={styles.option__subtitle}>{option.fullName}</span>
       </div>
-      <img className={styles.option__img} src={option.flag} />
+      <img
+        className={styles.option__img}
+        src={option.flag}
+        alt={'country flag'}
+      />
     </div>
   );
 };
